dc9c9144d3abf98de16dce85abb9f7769737c887
Jonathan Coe <jon@jbcoe.net>
2015-02-26 23:04:33 +0000

    started work on member functions for reference impl

---
+++ reference_implementation/propagate_const.hpp
@@ -0,0 +1,278 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+#include <functional>
+
+namespace std
+{
+namespace experimental
+{
+inline namespace fundamentals_v2
+{
+
+template <class T>
+class propagate_const
+{
+
+public:
+  typedef remove_reference_t<decltype(*declval<T&>())> element_type;
+
+private:
+  template <class U>
+  element_type* get_pointer(const U* u)
+  {
+    return u;
+  }
+
+  template <class U>
+  element_type* get_pointer(const U& u)
+  {
+    return get_pointer(u.get());
+  }
+
+  enum
+  {
+    IS_NOEXCEPT_SWAP = noexcept(swap(declval<T&>(), declval<T&>()))
+  };
+
+  enum
+  {
+    HAS_PTR_CONVERSION = is_convertible<T, element_type*>::value
+  };
+
+  enum
+  {
+    HAS_CPTR_CONVERSION = is_convertible<const T, const element_type*>::value
+  };
+
+  template <class U>
+  struct is_propagate_const : false_type
+  {
+  };
+
+  template <class U>
+  struct is_propagate_const<propagate_const<U>> : true_type
+  {
+  };
+
+  template <class U>
+  using is_propagate_const_v = is_propagate_const<U>::value;
+
+public:
+  // [propagate_const.ctor], constructors
+  constexpr propagate_const() = default;
+
+  propagate_const(const propagate_const& p) = delete;
+
+  constexpr propagate_const(propagate_const&& p) = default;
+
+  template <class U>
+  constexpr propagate_const(propagate_const<U>&& pu)
+      : t_(std::move(get_underlying(pu)))
+  {
+  }
+
+  template <class U, class V = enable_if_t<is_propagate_const_v<decay_t<U>>>>
+  constexpr propagate_const(U&& u)
+      : t_(std::forward<U>(u))
+  {
+  }
+
+  // [propagate_const.assignment], assignment
+  propagate_const& operator=(const propagate_const& p) = delete;
+
+  constexpr propagate_const& operator=(propagate_const&& p) = default;
+
+  template <class U>
+  constexpr propagate_const& operator=(propagate_const<U>&& pu)
+  {
+    t_ = std::move(pu.t_);
+    return *this;
+  }
+
+  template <class U, class V = enable_if_t<is_propagate_const_v<decay_t<U>>>>
+  constexpr propagate_const& operator=(U&& u)
+  {
+    t_ = std::move(pu.t_);
+    return *this;
+  }
+
+  // [propagate_const.const_observers], const observers
+  explicit constexpr operator bool() const
+  {
+    return get() != nullptr;
+  }
+  constexpr const element_type* operator->() const
+  {
+    return get();
+  }
+
+  template <class U = enable_if_t<HAS_CPTR_CONVERSION>>
+  constexpr operator const element_type*() const // Not always defined
+  {
+    return get() :
+  }
+
+  constexpr const element_type& operator*() const
+  {
+    return *get();
+  }
+
+  constexpr const element_type* get() const
+  {
+    return get_const_pointer(t_);
+  }
+
+  // [propagate_const.non_const_observers], non-const observers
+  constexpr element_type* operator->()
+  {
+    return get();
+  }
+
+  template <class U = enable_if_t<HAS_PTR_CONVERSION>>
+  constexpr operator element_type*()
+  {
+    return get() :
+  }
+
+  constexpr element_type& operator*()
+  {
+    return *get();
+  }
+  constexpr element_type* get()
+  {
+    return get_pointer(t_);
+  }
+
+  // [propagate_const.modifiers], modifiers
+  constexpr void swap(propagate_const& pt) noexcept(IS_NOEXCEPT_SWAP)
+  {
+    swap(t_, pt.t_);
+  }
+
+private:
+  T t_;
+};
+
+/*
+// [propagate_const.relational], relational operators
+template <class T>
+constexpr bool operator==(const propagate_const<T>& pt, nullptr_t);
+template <class T>
+constexpr bool operator==(nullptr_t, const propagate_const<T>& pu);
+
+template <class T>
+constexpr bool operator!=(const propagate_const<T>& pt, nullptr_t);
+template <class T>
+constexpr bool operator!=(nullptr_t, const propagate_const<T>& pu);
+
+template <class T, class U>
+constexpr bool operator==(const propagate_const<T>& pt,
+                          const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator!=(const propagate_const<T>& pt,
+                          const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator<(const propagate_const<T>& pt,
+                         const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator>(const propagate_const<T>& pt,
+                         const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator<=(const propagate_const<T>& pt,
+                          const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator>=(const propagate_const<T>& pt,
+                          const propagate_const<U>& pu);
+
+template <class T, class U>
+constexpr bool operator==(const propagate_const<T>& pt, const U& u);
+template <class T, class U>
+constexpr bool operator!=(const propagate_const<T>& pt, const U& u);
+template <class T, class U>
+constexpr bool operator<(const propagate_const<T>& pt, const U& u);
+template <class T, class U>
+constexpr bool operator>(const propagate_const<T>& pt, const U& u);
+template <class T, class U>
+constexpr bool operator<=(const propagate_const<T>& pt, const U& u);
+template <class T, class U>
+constexpr bool operator>=(const propagate_const<T>& pt, const U& u);
+
+template <class T, class U>
+constexpr bool operator==(const T& t, const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator!=(const T& t, const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator<(const T& t, const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator>(const T& t, const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator<=(const T& t, const propagate_const<U>& pu);
+template <class T, class U>
+constexpr bool operator>=(const T& t, const propagate_const<U>& pu);
+
+// [propagate_const.algorithms], specialized algorithms
+template <class T>
+constexpr void swap(propagate_const<T>& pt,
+                    propagate_const<T>& pt2) noexcept(IS_NOEXCEPT_SWAP);
+
+// [propagate_const.underlying], underlying pointer access
+template <class T>
+constexpr const T& get_underlying(const propagate_const<T>& pt) noexcept;
+template <class T>
+constexpr T& get_underlying(propagate_const<T>& pt) noexcept;
+} //  end namespace fundamentals_v2
+} //  end namespace experimental
+
+// [propagate_const.hash], hash support
+template <class T>
+struct hash;
+template <class T>
+struct hash<experimental::fundamentals_v2::propagate_const<T>> : hash<T>
+{
+};
+
+// [propagate_const.comparison_function_objects], comparison function objects
+template <class T>
+struct equal_to;
+template <class T>
+struct equal_to<experimental::fundamentals_v2::propagate_const<T>> : equal_to<T>
+{
+};
+template <class T>
+struct not_equal_to;
+template <class T>
+struct not_equal_to<experimental::fundamentals_v2::propagate_const<T>>
+    : not_equal_to<T>
+{
+};
+template <class T>
+struct less;
+template <class T>
+struct less<experimental::fundamentals_v2::propagate_const<T>> : less<T>
+{
+};
+template <class T>
+struct greater;
+template <class T>
+struct greater<experimental::fundamentals_v2::propagate_const<T>> : greater<T>
+{
+};
+template <class T>
+struct less_equal;
+template <class T>
+struct less_equal<experimental::fundamentals_v2::propagate_const<T>>
+    : less_equal<T>
+{
+};
+template <class T>
+struct greater_equal;
+template <class T>
+struct greater_equal<experimental::fundamentals_v2::propagate_const<T>>
+    : greater_equal<T>
+{
+};*/
+
+} // end namespace std
+
