649fdba612ffa3b39401d0ccb32e13c6707173f7
Jonathan B. Coe <jbcoe@me.com>
2018-03-13 21:09:46 -0400

    run clang-format on tests

--- test_propagate_const.cpp
+++ test_propagate_const.cpp
@@ -6,107 +6,94 @@ using std::experimental::propagate_const;
 
 enum { CONST_METHOD, NON_CONST_METHOD };
 
-struct A 
-{
-  auto method() const { return CONST_METHOD; }  
-  auto method() { return NON_CONST_METHOD; }  
+struct A {
+  auto method() const { return CONST_METHOD; }
+  auto method() { return NON_CONST_METHOD; }
 };
 
-#define REQUIRE_RETURN_TYPE_MATCH(expr,type) \
-  static_assert((std::is_same<decltype(expr), type>::value), #expr"->"#type); 
+#define REQUIRE_RETURN_TYPE_MATCH(expr, type)                          \
+  static_assert((std::is_same<decltype(expr), type>::value), #expr "-" \
+                                                                   ">" #type);
 
 //
 // plain pointer
 //
-TEST_CASE("Plain pointer operator ->", "[ptr->]")
-{
+TEST_CASE("Plain pointer operator ->", "[ptr->]") {
   A a;
   propagate_const<A*> pc_a(&a);
   REQUIRE(pc_a->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.operator->(), A*);
 }
 
-TEST_CASE("const Plain pointer operator ->", "[cptr->]")
-{
+TEST_CASE("const Plain pointer operator ->", "[cptr->]") {
   A a;
   const propagate_const<A*> cpc_a(&a);
   REQUIRE(cpc_a->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.operator->(), const A*);
 }
 
-TEST_CASE("Plain pointer get", "[ptr.get]")
-{
+TEST_CASE("Plain pointer get", "[ptr.get]") {
   A a;
   propagate_const<A*> pc_a(&a);
   REQUIRE(pc_a.get()->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const Plain pointer get", "[cptr.get]")
-{
+TEST_CASE("const Plain pointer get", "[cptr.get]") {
   A a;
   const propagate_const<A*> cpc_a(&a);
   REQUIRE(cpc_a.get()->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
 }
 
-TEST_CASE("Plain pointer operator *", "[ptr_op_*]")
-{
+TEST_CASE("Plain pointer operator *", "[ptr_op_*]") {
   A a;
   propagate_const<A*> pc_a(&a);
   REQUIRE((*pc_a).method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const Plain pointer operator *", "[cptr_op_*]")
-{
+TEST_CASE("const Plain pointer operator *", "[cptr_op_*]") {
   A a;
   const propagate_const<A*> cpc_a(&a);
   REQUIRE((*cpc_a).method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
 }
 
-
 //
 // unique_ptr
 //
-TEST_CASE("unique_ptr operator ->", "[uptr->]")
-{
+TEST_CASE("unique_ptr operator ->", "[uptr->]") {
   propagate_const<std::unique_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE(pc_a->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.operator->(), A*);
 }
 
-TEST_CASE("const unique_ptr operator ->", "[cuptr->]")
-{
+TEST_CASE("const unique_ptr operator ->", "[cuptr->]") {
   const propagate_const<std::unique_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE(cpc_a->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.operator->(), const A*);
 }
 
-TEST_CASE("unique_ptr get", "[uptr.get]")
-{
+TEST_CASE("unique_ptr get", "[uptr.get]") {
   propagate_const<std::unique_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE(pc_a.get()->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const unique_ptr get", "[cuptr.get]")
-{
+TEST_CASE("const unique_ptr get", "[cuptr.get]") {
   const propagate_const<std::unique_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE(cpc_a.get()->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
 }
 
-TEST_CASE("unique_ptr operator *", "[uptr_op_*]")
-{
+TEST_CASE("unique_ptr operator *", "[uptr_op_*]") {
   propagate_const<std::unique_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE((*pc_a).method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const unique_ptr operator *", "[cuptr_op_*]")
-{
+TEST_CASE("const unique_ptr operator *", "[cuptr_op_*]") {
   const propagate_const<std::unique_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE((*cpc_a).method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
@@ -115,60 +102,52 @@ TEST_CASE("const unique_ptr operator *", "[cuptr_op_*]")
 //
 // shared_ptr
 //
-TEST_CASE("shared_ptr operator ->", "[sptr->]")
-{
+TEST_CASE("shared_ptr operator ->", "[sptr->]") {
   propagate_const<std::shared_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE(pc_a->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.operator->(), A*);
 }
 
-TEST_CASE("const shared_ptr operator ->", "[csptr->]")
-{
+TEST_CASE("const shared_ptr operator ->", "[csptr->]") {
   const propagate_const<std::shared_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE(cpc_a->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.operator->(), const A*);
 }
 
-TEST_CASE("shared_ptr get", "[sptr.get]")
-{
+TEST_CASE("shared_ptr get", "[sptr.get]") {
   propagate_const<std::shared_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE(pc_a.get()->method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const shared_ptr get", "[csptr.get]")
-{
+TEST_CASE("const shared_ptr get", "[csptr.get]") {
   const propagate_const<std::shared_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE(cpc_a.get()->method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
 }
 
-TEST_CASE("shared_ptr operator *", "[sptr_op_*]")
-{
+TEST_CASE("shared_ptr operator *", "[sptr_op_*]") {
   propagate_const<std::shared_ptr<A>> pc_a(std::make_unique<A>());
   REQUIRE((*pc_a).method() == NON_CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(pc_a.get(), A*);
 }
 
-TEST_CASE("const shared_ptr operator *", "[csptr_op_*]")
-{
+TEST_CASE("const shared_ptr operator *", "[csptr_op_*]") {
   const propagate_const<std::shared_ptr<A>> cpc_a(std::make_unique<A>());
   REQUIRE((*cpc_a).method() == CONST_METHOD);
   REQUIRE_RETURN_TYPE_MATCH(cpc_a.get(), const A*);
 }
 
 size_t a_hash_calls = 0;
-template<> struct std::hash<A*>  
-{
-  size_t operator()(const A*) const
-  {
+template <>
+struct std::hash<A*> {
+  size_t operator()(const A*) const {
     ++a_hash_calls;
     return 0;
   }
 };
 
-TEST_CASE("hash", "[hash]")
-{
+TEST_CASE("hash", "[hash]") {
   A a;
   propagate_const<A*> pa(&a);
   a_hash_calls = 0;
@@ -177,129 +156,114 @@ TEST_CASE("hash", "[hash]")
 }
 
 size_t a_equal_to_calls = 0;
-template<> struct std::equal_to<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::equal_to<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_equal_to_calls;
     return false;
   }
 };
-TEST_CASE("equal_to", "[equal_to]")
-{
+TEST_CASE("equal_to", "[equal_to]") {
   A a;
   propagate_const<A*> pa(&a);
   a_equal_to_calls = 0;
-  std::equal_to<propagate_const<A*>>()(pa,pa);
+  std::equal_to<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_equal_to_calls == 1);
 }
 
 size_t a_not_equal_to_calls = 0;
-template<> struct std::not_equal_to<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::not_equal_to<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_not_equal_to_calls;
     return false;
   }
 };
-TEST_CASE("not_equal_to", "[not_equal_to]")
-{
+TEST_CASE("not_equal_to", "[not_equal_to]") {
   A a;
   propagate_const<A*> pa(&a);
   a_not_equal_to_calls = 0;
-  std::not_equal_to<propagate_const<A*>>()(pa,pa);
+  std::not_equal_to<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_not_equal_to_calls == 1);
 }
 
 size_t a_greater_calls = 0;
-template<> struct std::greater<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::greater<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_greater_calls;
     return false;
   }
 };
-TEST_CASE("greater", "[greater]")
-{
+TEST_CASE("greater", "[greater]") {
   A a;
   propagate_const<A*> pa(&a);
   a_greater_calls = 0;
-  std::greater<propagate_const<A*>>()(pa,pa);
+  std::greater<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_greater_calls == 1);
 }
 
 size_t a_less_calls = 0;
-template<> struct std::less<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::less<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_less_calls;
     return false;
   }
 };
-TEST_CASE("less", "[less]")
-{
+TEST_CASE("less", "[less]") {
   A a;
   propagate_const<A*> pa(&a);
   a_less_calls = 0;
-  std::less<propagate_const<A*>>()(pa,pa);
+  std::less<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_less_calls == 1);
 }
 
 size_t a_greater_equal_calls = 0;
-template<> struct std::greater_equal<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::greater_equal<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_greater_equal_calls;
     return false;
   }
 };
-TEST_CASE("greater_equal", "[greater_equal]")
-{
+TEST_CASE("greater_equal", "[greater_equal]") {
   A a;
   propagate_const<A*> pa(&a);
   a_greater_equal_calls = 0;
-  std::greater_equal<propagate_const<A*>>()(pa,pa);
+  std::greater_equal<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_greater_equal_calls == 1);
 }
 
 size_t a_less_equal_calls = 0;
-template<> struct std::less_equal<A*> 
-{
-  bool operator()(const A*, const A*) const
-  {
+template <>
+struct std::less_equal<A*> {
+  bool operator()(const A*, const A*) const {
     ++a_less_equal_calls;
     return false;
   }
 };
-TEST_CASE("less_equal", "[less_equal]")
-{
+TEST_CASE("less_equal", "[less_equal]") {
   A a;
   propagate_const<A*> pa(&a);
   a_less_equal_calls = 0;
-  std::less_equal<propagate_const<A*>>()(pa,pa);
+  std::less_equal<propagate_const<A*>>()(pa, pa);
   REQUIRE(a_less_equal_calls == 1);
 }
 
-class FakePtr
-{
-  public:
-  auto operator->() const { return this; }
+class FakePtr {
+ public:
+  auto operator-> () const { return this; }
   auto get() const { return this; }
   auto operator*() const { return *this; }
 };
 
 size_t fPtr_equals_fPtr_calls = 0;
-bool operator == (const FakePtr& f, const FakePtr& g)
-{
+bool operator==(const FakePtr& f, const FakePtr& g) {
   ++fPtr_equals_fPtr_calls;
   return false;
 }
-TEST_CASE("equals function", "[free ==]")
-{
+TEST_CASE("equals function", "[free ==]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_equals_fPtr_calls = 0;
@@ -308,13 +272,11 @@ TEST_CASE("equals function", "[free ==]")
 }
 
 size_t fPtr_not_equals_fPtr_calls = 0;
-bool operator != (const FakePtr& f, const FakePtr& g)
-{
+bool operator!=(const FakePtr& f, const FakePtr& g) {
   ++fPtr_not_equals_fPtr_calls;
   return false;
 }
-TEST_CASE("not equals function", "[free !=]")
-{
+TEST_CASE("not equals function", "[free !=]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_not_equals_fPtr_calls = 0;
@@ -323,13 +285,11 @@ TEST_CASE("not equals function", "[free !=]")
 }
 
 size_t fPtr_less_fPtr_calls = 0;
-bool operator < (const FakePtr& f, const FakePtr& g)
-{
+bool operator<(const FakePtr& f, const FakePtr& g) {
   ++fPtr_less_fPtr_calls;
   return false;
 }
-TEST_CASE("less function", "[free <]")
-{
+TEST_CASE("less function", "[free <]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_less_fPtr_calls = 0;
@@ -338,13 +298,11 @@ TEST_CASE("less function", "[free <]")
 }
 
 size_t fPtr_greater_fPtr_calls = 0;
-bool operator > (const FakePtr& f, const FakePtr& g)
-{
+bool operator>(const FakePtr& f, const FakePtr& g) {
   ++fPtr_greater_fPtr_calls;
   return false;
 }
-TEST_CASE("greater function", "[free >]")
-{
+TEST_CASE("greater function", "[free >]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_greater_fPtr_calls = 0;
@@ -353,13 +311,11 @@ TEST_CASE("greater function", "[free >]")
 }
 
 size_t fPtr_less_or_equal_fPtr_calls = 0;
-bool operator <= (const FakePtr& f, const FakePtr& g)
-{
+bool operator<=(const FakePtr& f, const FakePtr& g) {
   ++fPtr_less_or_equal_fPtr_calls;
   return false;
 }
-TEST_CASE("less or equal function", "[free <=]")
-{
+TEST_CASE("less or equal function", "[free <=]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_less_or_equal_fPtr_calls = 0;
@@ -368,13 +324,11 @@ TEST_CASE("less or equal function", "[free <=]")
 }
 
 size_t fPtr_greater_or_equal_fPtr_calls = 0;
-bool operator >= (const FakePtr& f, const FakePtr& g)
-{
+bool operator>=(const FakePtr& f, const FakePtr& g) {
   ++fPtr_greater_or_equal_fPtr_calls;
   return false;
 }
-TEST_CASE("greater or equal function", "[free >=]")
-{
+TEST_CASE("greater or equal function", "[free >=]") {
   propagate_const<FakePtr> f;
   propagate_const<FakePtr> g;
   fPtr_greater_or_equal_fPtr_calls = 0;
