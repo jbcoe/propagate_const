0724191e12475a1eddc11803727e5914e5197600
Antony Peacock <ant.peacock@gmail.com>
2019-10-11 15:34:14 +0100

    Add support for Conan packaging

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, CMake, tools
+
+
+class PropagateconstConan(ConanFile):
+    name = "propagate_const"
+    version = "1.0"
+    license = "MIT"
+    url = "https://github.com/jbcoe/propagate_const"
+    description = "<Description of Propagateconst here>"
+    topics = ("conan", "propagate_const", "header-only", "std", "experimental")
+    exports_sources = '*'
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.definitions["ENABLE_BUILDING_TESTS"] = "OFF"
+        cmake.configure()
+        cmake.install()
+
+    def package_id(self):
+        self.info.header_only()
