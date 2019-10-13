cdc0d3e2e0bbd30a20be1f2034161de70e7f5ab9
Antony Peacock <ant.peacock@gmail.com>
2019-10-13 22:45:22 +0100

    Correct example usage

--- .conan/test_package/test_package.cpp
+++ .conan/test_package/test_package.cpp
@@ -3,19 +3,24 @@
 #include <iostream>
 #include <stdexcept>
 
-class test {
+class component {
 public:
-    test() : m_message(std::make_unique<std::string>("The const version of message has been called")){}
-
     [[noreturn]] std::string message() { throw std::runtime_error("Do not call the non-const version of this function"); }
-    const std::string& message() const { return *m_message; }
+    std::string message() const { return "The const version of message has been called"; }
+};
+
+class owner {
+public:
+    owner() : m_component(std::make_unique<component>()){}
+
+    std::string message() const { return m_component->message(); }
 private:
-    std::experimental::propagate_const<std::unique_ptr<std::string>> m_message;
+    std::experimental::propagate_const<std::unique_ptr<component>> m_component;
 };
 
 int main()
 {
-    const test pointer_owner;
-    std::cout << pointer_owner.message();
+    const owner pointer_owner;
+    std::cout << pointer_owner.message() << std::endl;
     return EXIT_SUCCESS;
 }
